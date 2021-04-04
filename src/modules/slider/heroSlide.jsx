a282c7c25392bff09523390f83aea9c9b4574ded
viktormoskalev07 <neonchilkk@gmail.com>
2021-04-04 15:01:40 +0300

    Updates

--- src/modules/slider/heroSlide.jsx
+++
@@ -1,9 +0,0 @@
-const heroSlide = (p) => {
-    return (
-        <figure className="hero-slide__item">
-            <img src={p.img} alt="img"/>
-        </figure>
-    );
-}
-
-export default heroSlide;
